0952a5e7e2a496057c88dbc3304ba1d78af1730f
SarahLisleyy <135844091+SarahLisleyy@users.noreply.github.com>
2024-04-01 22:48:26 -0300

    Create ContadorContext.jsx

---
+++ ContadorContext.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState} from "react";
+
+const Contador = createContext()
+
+export const useContadorContext = () => {
+    return useContext(Contador)
+}
+
+export const ContadorProvedor = ({children}) => {
+    const [ContadorNumber, setContadorNumber] = useState(0)
+
+    const incrementarContador = () => {
+        setContadorNumber((setContador) => setContador + 1);
+    };
+
+    return(
+        <Contador.Provider value ={ {useContadorContext,incrementarContador}}>
+            {children}
+        </Contador.Provider>
+    )
+}
+export default [Contador, useContadorContext, ContadorProvedor]
