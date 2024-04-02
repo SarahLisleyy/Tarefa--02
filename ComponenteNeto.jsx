5fb274684312c05acf020b1c781a7d580e2d7e83
SarahLisleyy <135844091+SarahLisleyy@users.noreply.github.com>
2024-04-01 22:50:09 -0300

    Update ComponenteNeto.jsx

--- ComponenteNeto.jsx
+++ ComponenteNeto.jsx
@@ -1,18 +1,21 @@
 import React, { createContext, useContext, useState} from "react";
 import ComponenteAvo from "./ComponenteAvo";
 import ComponenteFilho from "./ComponenteFilho";
+import ContadorContext from "./ContadorContext";
+import { useCounterContext } from "./CounterContext";
 
 const {pokemonNumber} = createContext();
 const pokemonNumberNeto = pokemonNumber + 2;
+
 const ComponenteNeto = () => {
+    const { contador, incrementarContador } = useCounterContext();
 
     return (
         <div>
             <h1>Componente Neto</h1>
-            <img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%7BN%C3%9AMERO%7D.png'}
-                alt ={`Pokemon ${pokemonNumber + 2}`} />
+            <p>Contador: {contador}</p>
+            <button onClick={incrementarContador}>Incrementar</button>
         </div>
-
     )
 }
 
