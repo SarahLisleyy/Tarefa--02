b1f0668bebce88360370a33a58708f0d432afc7a
SarahLisleyy <135844091+SarahLisleyy@users.noreply.github.com>
2024-04-01 21:20:39 -0300

    Create FunctionContextA.jsx

---
+++ FunctionContextA.jsx
@@ -0,0 +1,16 @@
+import FunctionContextB from "./FunctionContextB"
+import FunctionContextC from "./FunctionContextC"
+import ColorTheme from "./MyColorContext"
+
+const cores = {bkgfA: 'green', bkgfB:'white', bkgC:'yellow', bkgD:'blue'}
+
+const FunctionContextA = () => {
+    return (
+        <ColorTheme.Provider value="cores">
+            <FunctionContextB />
+            <FunctionContextC />
+        </ColorTheme.Provider>
+    )
+}
+
+export default FunctionContextA
